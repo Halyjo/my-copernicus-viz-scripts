a6d68155ffeb89366e6f87294d6164a590879881
Halyjo <35629262+Halyjo@users.noreply.github.com>
2025-08-22 09:42:39 +0200

    add example water roughness script from sentinel-hub.

---
+++ water-surface-roughness.js
@@ -0,0 +1,7 @@
+// Copied from https://custom-scripts.sentinel-hub.com/sentinel-1/water_surface_roughness_visualization/
+// Water Surface Roughness Visualization 
+// Author: Annamaria Luongo (Twitter: @annamaria_84, https://www.linkedin.com/in/annamaria-luongo-RS) 
+// License: CC BY 4.0 International - https://creativecommons.org/licenses/by/4.0/ 
+
+var val = Math.log(0.05 / (0.018 + VV * 1.5));
+return [val]; 
